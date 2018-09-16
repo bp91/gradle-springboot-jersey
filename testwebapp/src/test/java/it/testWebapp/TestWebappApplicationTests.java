3f1fe85ccf94a619580249a7b838458c75e88eb4
= <=>
2018-09-16 17:38:46 +0200

    first commit

---
+++ testwebapp/src/test/java/it/testWebapp/TestWebappApplicationTests.java
@@ -0,0 +1,16 @@
+package it.testWebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestWebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
