ccf1dfdea3fb751cb78c47ce05364ddc1da4b668
= <=>
2018-09-17 23:49:06 +0200

    #? pgsql container

--- testwebapp/src/main/java/it/testWebapp/ApplicationTestWebapp.java
+++ testwebapp/src/main/java/it/testWebapp/ApplicationTestWebapp.java
@@ -1,9 +1,11 @@
 package it.testWebapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class ApplicationTestWebapp extends SpringBootServletInitializer {
@@ -16,4 +18,7 @@ public class ApplicationTestWebapp extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationTestWebapp.class, args);
     }
+    
+    @Autowired
+    private Environment env;
 }
