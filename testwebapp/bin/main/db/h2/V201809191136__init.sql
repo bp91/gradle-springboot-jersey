c176ab150d7aa4008f9d40620c8d4b26d9cda359
Bruno Pistone <brn.pistone@gmail.com>
2018-09-19 12:03:03 +0200

    #? test element into database

--- testwebapp/bin/main/db/h2/V201809191136__init.sql
+++ testwebapp/bin/main/db/h2/V201809191136__init.sql
@@ -1,22 +1,15 @@
-DROP TABLE IF EXISTS public.user;
-DROP TABLE IF EXISTS public.users;
-
-CREATE TABLE public.user (
+CREATE TABLE user (
     id SERIAL NOT NULL,
     creationtime TIMESTAMP WITHOUT TIME ZONE,
     name VARCHAR(255),
     surname VARCHAR(255),
-    born_date TIMESTAMP WITHOUT TIME ZONE,
-
-    CONSTRAINT user_pk PRIMARY KEY (name, surname)
+    born_date TIMESTAMP WITHOUT TIME ZONE
 );
 
-CREATE TABLE public.users (
+CREATE TABLE users (
     id serial NOT NULL,
     creationtime TIMESTAMP WITHOUT TIME ZONE,
     name character varying(255),
     surname character varying(255),
-    email character varying(255) NOT NULL,
-
-    CONSTRAINT users_pk PRIMARY KEY (email)
+    email character varying(255) NOT NULL
 );
--- testwebapp/src/test/java/it/testWebapp/TestWebappApplicationTests.java
+++ testwebapp/src/test/java/it/testWebapp/TestWebappApplicationTests.java
@@ -2,16 +2,27 @@ package it.testWebapp;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.google.common.collect.ImmutableMap;
+
+import it.testWebapp.service.UserService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @TestPropertySource(locations="classpath:application-dev.properties")
 public class TestWebappApplicationTests {
+	
+	@Autowired
+    private UserService userService;
 
 	@Test
 	public void contextLoads() {
@@ -22,5 +33,26 @@ public class TestWebappApplicationTests {
 		int a = 1;
 		
 		assertEquals(a, 1);
+	}
+	
+	@Test
+	public void checkUserBrunoTest() {
+		
+		List<Map<String, Object>> users = this.userService.getUsers();
+		
+		String mapToCompare = "";
+		
+		for(Map<String, Object> map : users) {
+			if(map.containsValue("brn.pistone@gmail.com")) {
+				mapToCompare = map.toString();
+			}
+		}
+		
+		assertEquals(mapToCompare, ImmutableMap.of(
+				"SURNAME", "Pistone",
+				"CREATIONTIME", "2018-09-19 08:37:18.466773",
+				"ID", "1",
+				"EMAIL", "brn.pistone@gmail.com",
+				"NAME", "Bruno").toString());
 	}
 }
