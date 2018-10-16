1b40adfbac0138d8c60f480584fde8e31a5921e2
bp91 <brn.pistone@gmail.com>
2018-10-16 17:47:23 +0200

    #? added gitignore

--- testwebapp/src/test/java/it/testWebapp/TestWebappApplicationTests.java
+++ testwebapp/src/test/java/it/testWebapp/TestWebappApplicationTests.java
@@ -2,16 +2,12 @@ package it.testWebapp;
 
 import static org.junit.Assert.assertEquals;
 import java.util.List;
-import java.util.Map;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.google.common.collect.ImmutableMap;
-
 import it.testWebapp.model.User;
 import it.testWebapp.service.UserService;
 
